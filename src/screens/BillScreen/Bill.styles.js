ca2b67ab764c26abf2ba02ffa8bc16417b71aec5
haunguyenphuc1110 <42568189+haunguyenphuc1110@users.noreply.github.com>
2019-11-21 15:48:26 +0700

    Add Bill screen

---
+++ src/screens/BillScreen/Bill.styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../assets';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  scroll: {
+    backgroundColor: COLORS.lightGray
+  },  
+  subContainer: {
+    marginBottom: 30,
+    backgroundColor: COLORS.white,
+    paddingHorizontal: 10,
+    paddingBottom: 10
+  },
+  button: {
+    height: 50,
+    backgroundColor: COLORS.appColor,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textButton: {
+    color: COLORS.white,
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
+
+export default styles;
