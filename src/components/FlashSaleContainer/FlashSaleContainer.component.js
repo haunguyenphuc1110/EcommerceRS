65a5c5e3c2dbd97dde052f58f09d3dc53a38f763
haunguyenphuc1110 <42568189+haunguyenphuc1110@users.noreply.github.com>
2019-11-23 17:31:13 +0700

    Add FlashSale component

---
+++ src/components/FlashSaleContainer/FlashSaleContainer.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+
+import FlashSaleItem from './FlashSaleItem/FlashSaleItem.component';
+
+import styles from './FlashSaleContainer.styles';
+
+const FlashSaleContainer = (props) => {
+  const { data } = props;
+  return (
+    <View style={styles.main}>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        numColumns={1}
+        data={data}
+        renderItem={({ item }) => (<FlashSaleItem item={item}/>)}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  )
+}
+
+export default FlashSaleContainer;
---
+++ src/components/FlashSaleContainer/FlashSaleContainer.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../assets';
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: COLORS.appColor,
+    elevation: 10
+  }
+});
+
+export default styles;
