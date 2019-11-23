89c36fd5701ad489f36c45731592c5d6ad7f4d4d
haunguyenphuc1110 <42568189+haunguyenphuc1110@users.noreply.github.com>
2019-11-23 17:24:22 +0700

    Add Loading component

--- src/components/Common/LoadingIndicator/Loading.styles.js
+++ src/components/Common/LoadingIndicator/Loading.styles.js
@@ -1,10 +1,42 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  actiIndicator: {
-    flex: 1, 
-    justifyContent: 'center', 
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  background: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
     alignItems: 'center'
+  },
+  textContainer: {
+    flex: 1,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute'
+  },
+  textContent: {
+    top: 80,
+    height: 50,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  activityIndicator: {
+    flex: 1
   }
 });
 
